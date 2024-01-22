fccd7dab32de86951c58be18eb854b4ccfa972ad
vdopov <crazywilly@yandex.ru>
2024-01-22 16:54:29 +0300

    Добавлен сценарий нагрузки

--- src/test/scala/perf/CommonScenario.scala
+++ src/test/scala/perf/CommonScenario.scala
@@ -13,30 +13,36 @@ class CommonScenario {
   exec(webtours)
   .exec(webtours2)
   .exec(webtours3)
+    .pause(1)
   }
   val login = group("login"){
   exec(webtours4)
     .exec(webtours5)
     .exec(webtours6)
+    .pause(1)
   }
   val openflights = group("openflights"){
     exec(webtours7)
       .exec(webtours8)
       .exec(webtours9)
+      .pause(1)
   }
   val ticketattributes = group ("ticketattributes"){
     exec(webtours10)
       .exec(webtours11)
       .exec(webtours12)
+      .pause(1)
   }
   val Itinerary = group("Itinerary") {
     exec(webtours13)
       .exec(webtours14)
       .exec(webtours15)
+      .pause(1)
   }
   val signoff = group("signoff") {
     exec(webtours16)
       .exec(webtours17)
+      .pause(1)
   }
   val mainScenario = scenario("mainScenario")
     .feed(Feeders.users)
